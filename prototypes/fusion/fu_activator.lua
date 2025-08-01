a2f96dfaba1a6c7ecce94681bfd8316990e20066
pla <massenpost@pla1.de>
2025-08-01 16:52:41 +0200

    Add circuit wire connectors to fu_* entities

--- prototypes/fusion/fu_activator.lua
+++ prototypes/fusion/fu_activator.lua
@@ -156,5 +156,15 @@ data:extend({
             sound = { filename = '__base__/sound/nuclear-reactor-1.ogg'},
             apparent_volume = 2.5,
         },
+    circuit_wire_max_distance = default_circuit_wire_max_distance,
+    circuit_connector = circuit_connector_definitions.create_vector(
+      universal_connector_template,
+      {
+        { variation =  0, main_offset = util.by_pixel( 42.75, -9.75), shadow_offset = util.by_pixel( 42.75, -9.75), show_shadow = true },
+        { variation =  0, main_offset = util.by_pixel( 42.75, -9.75), shadow_offset = util.by_pixel( 42.75, -9.75), show_shadow = true },
+        { variation =  0, main_offset = util.by_pixel( 42.75, -9.75), shadow_offset = util.by_pixel( 42.75, -9.75), show_shadow = true },
+        { variation =  0, main_offset = util.by_pixel( 42.75, -9.75), shadow_offset = util.by_pixel( 42.75, -9.75), show_shadow = true },
+      }
+    )
     },
 })
