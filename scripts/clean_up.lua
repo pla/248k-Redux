c2c59917209bc09a324689763f1e73b50d2cd12e
pla <massenpost@pla1.de>
2025-08-17 12:59:46 +0200

    Fix cleanup to properly hide items

--- scripts/clean_up.lua
+++ scripts/clean_up.lua
@@ -1,40 +1,33 @@
 local clean_up_list = {
-    "el_aluminum_ore_item",
-    "el_lithium_ore_item",
-    "el_ki_linker",
-    "fi_crushed_iron_item",
-    "fi_crushed_copper_item",
-    "fi_crushed_aluminum_item",
-    "fi_crushed_uranium_item",
-    "fi_industrial_steel_blend_item",
-    "fi_industrial_steel_item",
-    "fi_compound_material_item",
-    "fi_miner_item",
-    "fi_base_crystal_item",
-    "fi_catalyst_crystal_item"
+  "el_aluminum_ore",
+  "el_lithium_ore",
+  "el_ki_linker",
+  "fi_crushed_iron",
+  "fi_crushed_copper",
+  "fi_crushed_aluminum",
+  "fi_crushed_uranium",
+  "fi_industrial_steel_blend",
+  "fi_industrial_steel",
+  "fi_compound_material",
+  "fi_miner",
+  "fi_base_crystal",
+  "fi_catalyst_crystal",
 }
 --Encontre el error,en "data.raw.item[v].flags ="el "hidden" ya no es valido
 -- Que es lo que estaba intentando lograr con esta función?
 --Voy a ocultarlo por el momento para poder continuar con el resto del mod ya cuando pueda abrir el juego vere que hace esto
---[[
 
 local function clean_up(clean_up_list)
     for i,v in ipairs(clean_up_list) do
         if data.raw.item[v] then
-            data.raw.item[v].flags = { 
-                "hidden"
-             }
+            data.raw.item[v].hidden = true
         end
 
 --Encontre el error,en "data.raw["item-with-tags"][v].flags ="el "hidden" ya no es valido
         if data.raw["item-with-tags"][v] then
-            data.raw["item-with-tags"][v].flags = { 
-                "hidden"
-             }
+            data.raw["item-with-tags"][v].hidden = true
         end
     end
 end
 
 clean_up(clean_up_list)
-
-]]
