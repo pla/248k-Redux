c7e0a1563765860955da7b1b4c723779623e40b7
pla <massenpost@pla1.de>
2025-08-17 13:01:48 +0200

    simplyfied cleanup

--- scripts/clean_up.lua
+++ scripts/clean_up.lua
@@ -13,21 +13,13 @@ local clean_up_list = {
   "fi_base_crystal",
   "fi_catalyst_crystal",
 }
---Encontre el error,en "data.raw.item[v].flags ="el "hidden" ya no es valido
--- Que es lo que estaba intentando lograr con esta función?
---Voy a ocultarlo por el momento para poder continuar con el resto del mod ya cuando pueda abrir el juego vere que hace esto
 
-local function clean_up(clean_up_list)
-    for i,v in ipairs(clean_up_list) do
-        if data.raw.item[v] then
-            data.raw.item[v].hidden = true
-        end
+for i, v in ipairs(clean_up_list) do
+  if data.raw.item[v] then
+    data.raw.item[v].hidden = true
+  end
 
---Encontre el error,en "data.raw["item-with-tags"][v].flags ="el "hidden" ya no es valido
-        if data.raw["item-with-tags"][v] then
-            data.raw["item-with-tags"][v].hidden = true
-        end
-    end
+  if data.raw["item-with-tags"][v] then
+    data.raw["item-with-tags"][v].hidden = true
+  end
 end
-
-clean_up(clean_up_list)
